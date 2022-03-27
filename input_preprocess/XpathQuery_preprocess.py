e5a2a30ee56785883c563640d9d9c5e4030ba5d4
Suri Zhang <surizhang1224@gmail.com>
2022-03-27 15:07:55 +0800

    fix typo

--- input_preprocess/XpathQuery_preprocess.py
+++ input_preprocess/XpathQuery_preprocess.py
@@ -1,6 +1,9 @@
 import pymongo
 import re
 
+from handle_descendant import convert_all_descendant_to_child
+from handle_concise import short_hand_convert_to_full_path
+
 # returns a list of tuples (sanitized XPath query, function name)
 def data_preprocess(XpathQuery, isFullPath, database, collection):
     if (isFullPath == False):
@@ -21,18 +24,20 @@ def data_preprocess(XpathQuery, isFullPath, database, collection):
     # only match text() at the end of query
     text = re.compile('/(child::)?text\(\)')
     # match * at the end of query
-    any_element = re.complie('\*$')
-    
-    for xpath in xpath_set_2:   
+    any_element = re.compile('\*$')
+    for xpath in xpath_set_2:
         if count.match(xpath):
             sanitized_query = count.split(xpath)[1]
-            result.append(sanitized_query, "count")
-        if text.search(xpath):
+            result.append((sanitized_query, "count"))
+        elif text.search(xpath):
             sanitized_query = text.split(xpath)[0]
-            result.append(sanitized_query, "text")
-        if any_element.search(xpath):
+            result.append((sanitized_query, "text"))
+        elif any_element.search(xpath):
             sanitized_query = any_element.split(XpathQuery)[0][:-1]
             result.append(sanitized_query, "any_element")
         else:  # no function detected, return original XpathQuery and empty function name
-            result.append(xpath, "")
+            result.append((xpath, ""))
     return result
+
+
+print(data_preprocess("count(child::library/descendant::album[child::artists[child::artist/child::name='Anang Ashanty']]/child::artists/child::artist[child::country='Indonesia']/child::name[child::age>30])", True, "", ""))
